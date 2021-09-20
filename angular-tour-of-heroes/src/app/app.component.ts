cdc1305c3e6740a44b188ead0037f92c04a0f1b6
Logan Butler <LoganCButler@users.noreply.github.com>
2021-09-20 16:53:05 -0400

    Add heros component

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = "Logan's angular-tour-of-heroes";
 }
